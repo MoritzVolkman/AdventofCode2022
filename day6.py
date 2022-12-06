e0b0490f3d76c92e684b7ec59761688e2e251e59
acb017 <moeserdkunde@gmail.com>
2022-12-06 18:41:56 +0100

    Simplified Solution for Day 6

--- day6.py
+++ day6.py
@@ -1,31 +1,17 @@
-def part1():
+def part1(individual_chars):
     input = open("input.txt").read()
     last_chars = []
     position = 0
     for char in input:
         position += 1
         last_chars.append(char)
-        if position > 4:
+        if position > individual_chars:
             last_chars.pop(0)
-            if len(set(last_chars)) >= 4:
+            if len(set(last_chars)) >= individual_chars:
                 print("Start of Package Marker Detected, Position = ", position)
                 break
 
 
-def part2():
-    input = open("input.txt").read()
-    last_chars = []
-    position = 0
-    for char in input:
-        position += 1
-        last_chars.append(char)
-        if position > 14:
-            last_chars.pop(0)
-            if len(set(last_chars)) >= 14:
-                print("Start of Message Marker Detected, Position = ", position)
-                break
-
-
 if __name__ == "__main__":
-    part1()
-    part2()
+    part1(4)
+    part1(14)
